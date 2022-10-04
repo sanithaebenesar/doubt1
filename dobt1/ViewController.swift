5b23d72699eed3a12e45c16c3c0451698efd88e3
Surya Sudhakaran <joshual.joseph@codinghands.in>
2022-10-04 14:04:55 +0530

    initial commit

---
+++ dobt1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dobt1
+//
+//  Created by CH 002 on 02/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+var names = [["Abi","Akshara"],["Beena","Bini"],["Celin","Cera"]]
